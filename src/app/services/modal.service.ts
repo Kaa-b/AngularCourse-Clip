5e15d975b44650f985ac03187b9f0640b7776cf9
Kaa-b <marine.bauer5@gmail.com>
2022-11-11 18:10:46 +0100

    first commit

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+interface IModel {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  private modals: IModel[] = []
+
+  constructor() { }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    })
+  }
+
+  isModalOpen(id: string) : boolean {
+    // ! returns the opposite by converting it into a boolean, so !! just converts into a boolean
+    // Boolean(this.modals.find(element => element.id === id)?.visible) also works
+    return !!this.modals.find(element => element.id === id)?.visible
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+
+    if (modal) {
+      modal.visible = !modal.visible
+    }
+  }
+}
